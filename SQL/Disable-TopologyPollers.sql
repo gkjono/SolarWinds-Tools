14b61f25c6c87dd863a2ee91b214c3ea91fed546
Greg Kjono <gkjono@users.noreply.github.com>
2022-07-19 09:24:58 -0400

    Initial Commit of New Queries

---
+++ SQL/Disable-TopologyPollers.sql
@@ -0,0 +1,7 @@
+-- Scripts are provided AS IS without warranty of any kind.
+-- No warranty expressed or implied. 
+-- Use at your own risk.
+
+UPDATE Pollers
+SET [Enabled] = 0
+WHERE PollerType LIKE 'N.Topology%' AND [Enabled] <> 0
---
+++ SQL/Get-EnabledTopologyPollers.sql
@@ -0,0 +1,14 @@
+-- Scripts are provided AS IS without warranty of any kind.
+-- No warranty expressed or implied. 
+-- Use at your own risk.
+
+SELECT n.Caption
+	, n.IP_Address
+	, n.Vendor
+	, n.MachineType
+	, p.PollerID
+	, p.PollerType
+	, [Enabled] 
+FROM Pollers p
+INNER JOIN NodesData n ON n.NodeID=p.NetObjectID
+WHERE PollerType LIKE 'N.Topology%' AND [Enabled] = 1
